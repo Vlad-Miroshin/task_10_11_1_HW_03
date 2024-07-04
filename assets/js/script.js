b09fe113d7d22aaaf74b8baa0c3f7907e7697b59
Vlad Miroshin <ppvlad@mail.ru>
2024-07-04 12:07:40 +0300

    fix (sctript.js) dropdown algorithm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,17 @@
-const menuBtn = document.querySelector(".menu-btn");
-const closeBtn = document.querySelector(".close-btn");
-const sideBar = document.querySelector(".side-bar");
 const subBtn_1 = document.querySelector("#sub-btn-1");
 const subMenu_1 = document.querySelector("#sub-menu-1");
 const subBtn_2 = document.querySelector("#sub-btn-2");
 const subMenu_2 = document.querySelector("#sub-menu-2");
 
-menuBtn.addEventListener('click', function(e) {
-    sideBar.classList.add('active');
-    menuBtn.style.visibility = 'hidden';
-}); 
-
-closeBtn.addEventListener('click', function(e) {
-    sideBar.classList.remove('active');
-    menuBtn.style.visibility = 'visible';
-}); 
-
 subBtn_1.addEventListener('click', function(e) {
     toggle_display(subMenu_1);
+    this.querySelector('.dropdown').classList.toggle('rotate');
 }); 
 
 subBtn_2.addEventListener('click', function(e) {
     toggle_display(subMenu_2);
+
+    this.querySelector('.dropdown').classList.toggle('rotate');
 }); 
 
 function toggle_display(elem) {
